d54a86f9033ccc6cbd49be35044b7993d59befab
seydou Ouattara <s.ouattara@younup.fr>
2022-09-22 16:56:11 +0200

    (refactor) refactor to Utils, adding tests, adding unit test, limit the request between 1 and 100

--- src/main/java/com/seydou/fizzbuzzLike/controller/Controller.java
+++ src/main/java/com/seydou/fizzbuzzLike/controller/Controller.java
@@ -4,8 +4,6 @@ import com.seydou.fizzbuzzLike.utils.Utils;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import javax.print.attribute.standard.Media;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,6 +34,15 @@ public class Controller {
     }
 
 
+    /**
+     * Return a list contains results by post http methode
+     * @param int1 first multiple
+     * @param int2 second multiple
+     * @param limit limit of results
+     * @param str1 word to replace with first multiple matches
+     * @param str2 word to replace with first multiple matches
+     * @return result
+     */
      @PostMapping(value = "/attributePost", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
      public List<String> postFizzBuzz(@RequestParam  Integer int1,
                                       @RequestParam  Integer int2,
@@ -43,45 +50,61 @@ public class Controller {
                                       @RequestParam  String str1,
                                       @RequestParam  String str2){
 
+         // nettoyage
+         int1 = Utils.sanityzeMultibple(int1);
+         int2 = Utils.sanityzeMultibple(int2);
+         limit = Utils.sanityzeLimit(limit);
          // chaine servant de clef
-         String key = String.valueOf(int1)+ " - "
-                 + String.valueOf(int2)+ " - "
-                 + String.valueOf(limit)+ " - "
-                 + str1+ " - "
-                 + str2;
-         if(stats.containsKey(key)){
-             stats.replace(key,stats.get(key)+1);
-         }else{
-             stats.put(key, 1);
-         }
-
+         String key = Utils.getKeyByParam(int1,int2,limit, str1, str2);
+         // maj de la map
+         settingMapResult(key);
+         // on constitue le resultat à retourner
          List<String> list = Utils.getResultList(int1, int2, limit, str1, str2);
          return list;
      }
 
+
+
+    /**
+     * Return a list contains results by get http methode
+      * @param int1 first multiple
+     * @param int2 second multiple
+     * @param limit limit of results
+     * @param str1 word to replace with first multiple matches
+     * @param str2 word to replace with first multiple matches
+     * @return result
+     */
     @GetMapping("/attribute")
     public List<String> getFizzBuzz (@RequestParam Integer int1,
                                      @RequestParam Integer int2,
                                      @RequestParam Integer limit,
                                      @RequestParam String str1,
                                      @RequestParam String str2){
-
+        // nettoyage
+        int1 = Utils.sanityzeMultibple(int1);
+        int2 = Utils.sanityzeMultibple(int2);
+        limit = Utils.sanityzeLimit(limit);
         // chaine servant de clef
-        String key = String.valueOf(int1)+ " - "
-                + String.valueOf(int2)+ " - "
-                + String.valueOf(limit)+ " - "
-                + str1+ " - "
-                + str2;
-
-                if(stats.containsKey(key)){
-                    stats.replace(key,stats.get(key)+1);
-                }else{
-                    stats.put(key, 1);
-                }
-
+        String key = Utils.getKeyByParam(int1,int2,limit, str1, str2);
+        // maj de la map
+        settingMapResult(key);
+        // on constitue le resultat à retourner
         List<String> list = Utils.getResultList(int1, int2, limit, str1, str2);
         return list;
 
 
+    }
+
+
+    /**
+     * Set or update the map result
+     * @param key
+     */
+    private void settingMapResult(String key) {
+        if (stats.containsKey(key)) {
+            stats.replace(key, stats.get(key) + 1);
+        } else {
+            stats.put(key, 1);
+        }
     }
 }
